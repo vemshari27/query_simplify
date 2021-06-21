0d9539c4d227e964f9c05956623fc6a2e303dac8
Eric John <ericjohneastwood@gmail.com>
2021-06-21 16:43:55 +0530

    Clean Master

--- forms.py
+++
@@ -1,6 +0,0 @@
-from wtforms import StringField, Form, SubmitField
-from wtforms.validators import DataRequired
-
-class SearchForm(Form):
-  search = StringField('search', [DataRequired()])
-  submit = SubmitField('Search', render_kw={'class': 'btn btn-success btn-block'})
